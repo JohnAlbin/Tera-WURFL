4bd2c9d2fce4b95f04baae2771bf81a71e3fd04d
Steve Kamerman <stevekamerman@gmail.com>
2011-02-21 18:27:40 -0600

    Updated Description and README

--- DatabaseConnectors/TeraWurflDatabase_MSSQL2005.php
+++ DatabaseConnectors/TeraWurflDatabase_MSSQL2005.php
@@ -4,7 +4,7 @@
  * 
  * Tera-WURFL was written by Steve Kamerman, and is based on the
  * Java WURFL Evolution package by Luca Passani and WURFL PHP Tools by Andrea Trassati.
- * This version uses a MySQL database to store the entire WURFL file, multiple patch
+ * This version uses a database to store the entire WURFL file, multiple patch
  * files, and a persistent caching mechanism to provide extreme performance increases.
  * 
  * @package TeraWurflDatabase
